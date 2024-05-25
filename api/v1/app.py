e77c70b1f2042b5f844a04a25fe41015e55aede1
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-05-25 16:48:35 +0100

    Error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Module contains set up for flask application"""
 
-from flask import Flask, Blueprint, jsonify, make_response
+from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -18,7 +18,7 @@ def tear_down(exception):
 @app.errorhandler(404)
 def not_found(error):
     """ Returning custom error message """
-    return make_response(jsonify({"error": "Not found"}), 404)
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":

c478cffa20e8253ee1a715d406cab73b1a971157
Triad01 <luckypee01@gmail.com>
2024-05-26 14:34:15 +0100

    End point to show stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,27 @@
 """index file of our flask application"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.engine.db_storage import classes
 
 
 @app_views.route("/status")
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    amenities = storage.count(classes.get('Amenity'))
+    cities = storage.count(classes.get('City'))
+    places = storage.count(classes.get('Place'))
+    reviews = storage.count(classes.get('Review'))
+    states = storage.count(classes.get('State'))
+    users = storage.count(classes.get('User'))
+    return jsonify({
+                    "amenities": amenities,
+                    "cities": cities,
+                    "places": places,
+                    "reviews": reviews,
+                    "states": states,
+                    "users": users})

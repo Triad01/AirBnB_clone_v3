1e4d35c36876f6137eaecfac40fcea945c25f68a
Triad01 <luckypee01@gmail.com>
2024-05-27 12:05:42 +0100

    added HTTP access control

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,14 @@
 """ Module contains set up for flask application"""
 
 from flask import Flask, Blueprint, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
